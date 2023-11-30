2ce0fbaf51d8db7ebfb9dbfa356b29ee060ba316
khaticaa <128252286+khaticaa@users.noreply.github.com>
2023-11-30 11:36:44 +0400

    s3s4s5

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const bannerText=document.querySelector('.banner-text')
+const bannerText = document.querySelector(".banner-text");
 
-document.addEventListener('DOMContentLoaded', ()=>{
-  bannerText.style.transform = 'scale(1)'
-})
+document.addEventListener("DOMContentLoaded", () => {
+  bannerText.style.transform = "scale(1)";
+});
 
 document.addEventListener("click", () => {
   const modal = document.getElementById("myModal");
@@ -11,12 +11,10 @@ document.addEventListener("click", () => {
 
   const openModal = () => {
     modal.style.display = "block";
-  
   };
 
   const closeModal = () => {
     modal.style.display = "none";
-  
   };
 
   modalTriggerElements.forEach((element) => {
@@ -32,6 +30,55 @@ document.addEventListener("click", () => {
   });
 });
 
+const stuffedAnimals = [
+  { name: "Tedy Bear", price: "$10 USD", imageSrc: "s1left.png" },
+  {
+    name: "Mega Plush Toy",
+    price: "$88USD",
+    imageSrc:
+      "5bae124e03ef144f2b4a9bef_33837-2-plush-toy-transparent-background-min.png",
+  },
+  { name: "Cute Dog", price: "$30USD", imageSrc: "cutedog.png" },
+  { name: "Little Friend", price: "$31USD", imageSrc: "littlefriend.png" },
+];
 
+const container = document.querySelector("#section2 .s2-o .s2boxes");
 
+stuffedAnimals.forEach((animal) => {
+  const box = document.createElement("div");
+  box.classList.add("box");
 
+  box.innerHTML = `
+    <div class="bimg">
+      <img src="${animal.imageSrc}" alt="" />
+    </div>
+    <h3>${animal.name}</h3>
+    <button>${animal.price}</button>
+  `;
+
+  container.appendChild(box);
+});
+
+const stuffedAnimals1 = [
+  { name: "Happy Flowers", price: "$38.000USD", imageSrc: "s1right.png" },
+  { name: "Lift Machine", price: "$38.000USD", imageSrc: "liftmachine.png" },
+  { name: "Wodden Camera", price: "$38.000USD", imageSrc: "camera.png" },
+  { name: "LIttle Rabit", price: "$38.000USD", imageSrc: "littlerabbit.png" },
+];
+
+const boxes3 = document.querySelector(".s3boxes");
+
+stuffedAnimals1.forEach((item) => {
+  const box = document.createElement("div");
+  box.classList.add("box");
+
+  box.innerHTML = `
+      <div class="bimg">
+        <img src="${item.imageSrc}" alt="" />
+      </div>
+      <h3>${item.name}</h3>
+      <button>${item.price}</button>
+    `;
+
+  boxes3.appendChild(box);
+});
